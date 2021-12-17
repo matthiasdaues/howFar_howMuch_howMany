6d0d2ab3a313917674aaa3f81e0cb7ee0301275b
matthiasdaues <md@matthiasdaues.de>
2021-12-18 00:05:39 +0100

    December 18, 2021 12:05 AM

--- 01_sql/create_location_junction_and_connecting_edge.sql
+++ 01_sql/create_location_junction_and_connecting_edge.sql
@@ -17,19 +17,20 @@ CREATE OR REPLACE FUNCTION osm.create_location_junction(
 
 RETURNS TABLE (
 	addr_id bigint
+,   way_id bigint
 ,   addr_location_id bigint
 ,   junction_location_id bigint
---,   junction_edge geometry
---,   junction_node geometry
+,   junction_edge geometry
+,   junction_node geometry
 
 -- Test block ---------------------------------
 
-,   addr_distance numeric(10,2)
-,   addr_location geometry
-,   addr_location_reverse       geometry
-,   junction_distance numeric(10,2)
-,   junction_location geometry
-,   junction_location_reverse   geometry
+-- ,   addr_distance numeric(10,2)
+-- ,   addr_location geometry
+-- ,   addr_location_reverse       geometry
+-- ,   junction_distance numeric(10,2)
+-- ,   junction_location geometry
+-- ,   junction_location_reverse   geometry
 
 -----------------------------------------------
 
@@ -41,17 +42,20 @@ DECLARE
     
 	addr_geohash           text;     
     addr_location_id       bigint;   
+	way_geom_id            text[];
     way_geom               geometry;
+	way_id                 bigint;
 	junction_location      geometry; 
     junction_location_hash text;
     junction_location_id   bigint;
+	junction_edge          geometry;
+	junction_location_hash_reverse text;
+    junction_location_reverse      geometry;
 
  -- test block
 
-    addr_geohash_reverse   text;
-    addr_location_reverse  geometry;
-    junction_location_hash_reverse text;
-    junction_location_reverse      geometry;
+--     addr_geohash_reverse   text;
+--     addr_location_reverse  geometry;
 
 -----------------------------------------------------
 
@@ -60,21 +64,24 @@ BEGIN
 
     addr_geohash          := st_geohash(this_addr_geom,10);
     addr_location_id      := geohash_decode(addr_geohash);
-    select into way_geom 
-        way.geom::geometry
+    select into way_geom_id 
+        array[st_astext(way.geom),way.way_id::text]
         from osm.highways way
         order by this_addr_geom <-> way.geom 
-        limit 1; 
-    junction_location      := st_closestpoint(way_geom, this_addr_geom);
-    junction_location_hash := st_geohash(junction_location::geometry,10);
-    junction_location_id   := geohash_decode(junction_location_hash);
+        limit 1;
+	way_geom                  := st_setsrid(way_geom_id[1]::geometry,4326);
+	way_id                    := way_geom_id[2]::bigint;
+    junction_location         := st_closestpoint(way_geom, this_addr_geom);
+    junction_location_hash    := st_geohash(junction_location::geometry,10);
+    junction_location_id      := geohash_decode(junction_location_hash);
+	junction_location_hash_reverse := geohash_encode(junction_location_id);
+	junction_location_reverse := st_setsrid(st_centroid(st_geomfromgeohash(junction_location_hash_reverse)),4326)::geometry; -- will be junction_node in the result
+    junction_edge             := st_setsrid(st_makeline(this_addr_geom, junction_location_reverse),4326);
 
 -- test block for plausibility of hashing operation
 
-    addr_geohash_reverse           := geohash_encode(addr_location_id);
-	addr_location_reverse          := st_setsrid(st_centroid(st_geomfromgeohash(addr_geohash_reverse)),4326)::geometry;
-	junction_location_hash_reverse := geohash_encode(junction_location_id);
-	junction_location_reverse      := st_setsrid(st_centroid(st_geomfromgeohash(junction_location_hash_reverse)),4326)::geometry;
+--  addr_geohash_reverse           := geohash_encode(addr_location_id);
+-- 	addr_location_reverse          := st_setsrid(st_centroid(st_geomfromgeohash(addr_geohash_reverse)),4326)::geometry;
 	
 ----------------------------------------------------
 
@@ -82,19 +89,20 @@ BEGIN
     
     select
         this_addr_id as addr_id 
+	,   way_id
     ,   addr_location_id
     ,   junction_location_id 
---    ,   junction_edge 
---    ,   junction_node 
+    ,   junction_edge 
+    ,   junction_location_reverse as junction_node 
 
 -- Test block 
 
-    ,   st_distance(this_addr_geom::geography, addr_location_reverse::geography)::numeric(10,2) as addr_distance
-    ,   this_addr_geom as addr_location
-    ,   addr_location_reverse
-    ,   st_distance(junction_location::geography, junction_location_reverse::geography)::numeric(10,2) as junction_distance
-    ,   junction_location
-    ,   junction_location_reverse
+--     ,   st_distance(this_addr_geom::geography, addr_location_reverse::geography)::numeric(10,2) as addr_distance
+--     ,   this_addr_geom as addr_location
+--     ,   addr_location_reverse
+--     ,   st_distance(junction_location::geography, junction_location_reverse::geography)::numeric(10,2) as junction_distance
+--     ,   junction_location
+--     ,   junction_location_reverse
 	
 ---------------------------------------------
 
