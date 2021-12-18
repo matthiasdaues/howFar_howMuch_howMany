42ba223d45a63f4c07a283eb006575f397a0bb5d
matthiasdaues <md@matthiasdaues.de>
2021-12-19 00:03:29 +0100

    December 19, 2021 12:03 AM

---
+++ 01_sql/create_enhanced_way_segment_from_junctions_test.sql
@@ -0,0 +1,31 @@
+with grafts as (
+    select
+	    way_id
+--	,   array_agg(junction_location_id) as junction_location_id
+	,   st_union(junction_node) as nodes
+	,   st_union(junction_edge)
+	from
+	    osm.junctions
+	group by
+	    way_id
+	limit
+	    1
+    )
+select
+    st_snap(
+        res.geom
+	,   grafts.nodes
+	,   0.5
+	)::geometry as new_road
+from
+    grafts grafts
+join lateral
+    (select
+	    h.way_id
+	,   h.geom
+	from
+	    osm.highways h
+	where
+	    grafts.way_id = h.way_id
+	) res on true
+;
