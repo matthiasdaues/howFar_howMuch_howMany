bd4cb3f8653bebda6eac09b641e6e76fd729e7be
matthiasdaues <md@matthiasdaues.de>
2021-12-22 17:03:11 +0100

    December 22, 2021 5:03 PM

--- gis@localhost.session.sql
+++ gis@localhost.session.sql
@@ -2,6 +2,11 @@ select
     *
 from
     osm.boundaries
-where
-    tags ->> 'name' like '%amburg%'
+where (
+        tags ->> 'name' in ('%amburg%','%erlin%')
+    and 
+        tags ->> 'admin_level' = '4'
+    )
+-- or
+--     tags ->> 'admin_level' = '6'
 ;
