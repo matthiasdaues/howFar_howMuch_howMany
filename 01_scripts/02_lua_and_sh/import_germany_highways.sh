c120ed41a514145ee7697d4dcf310e9fa942af47
matthiasdaues <matthias.daues@datenschoenheit.de>
2023-03-16 16:06:42 +0000

    16 Mar 2023, 17:06

--- 01_scripts/02_lua_and_sh/import_germany_highways.sh
+++ 01_scripts/02_lua_and_sh/import_germany_highways.sh
@@ -1,19 +1,20 @@
 # Import von OSM-Daten über Osmium und osm2pgsql
 
-cd ~/Documents/geo_experimente/osm/OSM
+cd ~/Documents/geodata
 
-osmium tags-filter --overwrite -v -o berlin.osm.pbf berlin-latest.osm.pbf w/highway r/boundary=administrative rw/building:* nrw/addr:* nrw/type:building
+# the first filter expression filters on ways with the
+# tag "highway" where this has not the values given in
+# the comma separated list of terms
+osmium tags-filter -O -o ./data/germany_filtered_01.osm.pbf ./germany-latest.osm.pbf w/highway
+osmium tags-filter -O -o ./data/germany_filtered_02.osm.pbf ./data/germany_filtered_01.osm.pbf highway!=area,rest_area,footway,cycleway,parking,parking_aisle,bridleway,motorway,motorway_link,steps,track,trunk,trunk_link,path,abandoned,bus_guideway,construction,corridor,elevator,escalator,private,proposed,planned,platform,raceway,traffic_signals,crossing
 
-
-
-# osm2pgsql -H localhost -P 25432 -U gis -W -d gis -O flex -S highways.lua hamburg.osm.pbf
-osm2pgsql -H localhost -P 25432 -U gis -W -d gis -O flex -S buildings.lua germany.osm.pbf
-osm2pgsql -H localhost -P 25432 -U gis -W -d gis -O flex -S addr.lua germany.osm.pbf
-
+## the inverse of the sets having tunnel, foot, area, etc as tags is all
+## elements where neither is the case. They can be concatenated.
+osmium tags-filter -O -i -o ./data/germany_filtered_03.osm.pbf ./data/germany_filtered_02.osm.pbf n/crossing w/tunnel=yes n/barrier nw/area nw/bridge w/service=parking_aisle
 
 # test the file for missing objects
-osmium getid germany-latest.osm.pbf w43636167 -o test.osm.pbf
+osmium getid ./data/germany_filtered_03.osm.pbf w43636167 -o ./data/germany_filtered_04.osm.pbf
 
 # Import "generic"
-osm2pgsql -H localhost -P 25432 -U gis -W -d gis -O flex -S generic.lua /home/matthiasdaues/Documents/geo_experimente/osm/OSM/germany-latest.osm.pbf
+osm2pgsql -H datenschoenheit.de -P 25433 -U gis -W -d gis -O flex -S /home/matthiasdaues/Documents/projects/howFar_howMuch_howMany/01_scripts/02_lua_and_sh/highway.lua /home/matthiasdaues/Documents/geodata/data/germany_filtered_03.osm.pbf
 
